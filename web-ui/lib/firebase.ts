7856468005dea08c04c171e96def5fef650d8ec0
jsildura <sildura.joelito.t@gmail.com>
2026-02-05 16:01:59 +0800

    security: remove hardcoded Firebase credentials, use env vars only

--- web-ui/lib/firebase.ts
+++ web-ui/lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBPK8lEdCcoQwo3dMXq698AU-K7dQiUdGE",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "tidal-dl-ng-history.firebaseapp.com",
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "tidal-dl-ng-history",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "tidal-dl-ng-history.firebasestorage.app",
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "457378055598",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:457378055598:web:9ea0e176769cdae940343f"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase (prevent multiple initializations)
